254c0c658a50d51c74b5c21827bae832656a5b23
Arbaz Shoukat <arbazkamboh342@.com>
2025-02-22 00:36:09 +0500

    fix sitemap

--- src/helpers/generateSiteMap.ts
+++ src/helpers/generateSiteMap.ts
@@ -18,22 +18,21 @@ export interface OrganizedCountry {
 export function organizeCountries(
   countriesData: Country[],
 ): OrganizedCountry[] {
-  const countryMap: Record<string, Country[]> = {};
+  // Group countries by their first letter using reduce
+  const countryMap = countriesData.reduce<Record<string, Country[]>>(
+    (acc, country) => {
+      const firstLetter = country.name.charAt(0).toUpperCase();
+      if (!acc[firstLetter]) {
+        acc[firstLetter] = [];
+      }
+      acc[firstLetter].push(country);
+      return acc;
+    },
+    {},
+  );
 
-  countriesData.forEach((country) => {
-    const firstLetter = country.name.charAt(0).toUpperCase();
-
-    if (!countryMap[firstLetter]) {
-      countryMap[firstLetter] = [];
-    }
-
-    countryMap[firstLetter].push(country);
-  });
-
-  return Object.keys(countryMap)
-    .map((letter) => ({
-      letter,
-      countries: countryMap[letter],
-    }))
+  // Convert the grouped object to an array and sort it by the letter key
+  return Object.entries(countryMap)
+    .map(([letter, countries]) => ({ letter, countries }))
     .sort((a, b) => a.letter.localeCompare(b.letter));
 }
